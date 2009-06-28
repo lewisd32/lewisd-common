7a084789af7cec73981eb60dfc4bf7c150bda13a
lewisd <lewisd@4026beb2-63e4-11de-b47b-210fc227597e>
2009-06-28 13:23:16 +0000

    Reformatting code.

--- trunk/src/main/java/com/lewisd/test/Assert.java
+++ trunk/src/main/java/com/lewisd/test/Assert.java
@@ -7,67 +7,90 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
-import com.lewisd.reflection.ReflectionHelper;
-
 import junit.framework.AssertionFailedError;
 
-public class Assert extends junit.framework.Assert {
-    
+import com.lewisd.reflection.ReflectionHelper;
+
+public class Assert extends junit.framework.Assert
+{
+
     private static final List<String> REFLECTIVE_EQUALS_EXCLUDE_PACKAGES = new LinkedList<String>();
-    
-    static {
+
+    static
+    {
         REFLECTIVE_EQUALS_EXCLUDE_PACKAGES.add("java");
         REFLECTIVE_EQUALS_EXCLUDE_PACKAGES.add("javax");
         REFLECTIVE_EQUALS_EXCLUDE_PACKAGES.add("sun");
     }
-    
-    public static void assertEqualsReflectively(Object expected, Object actual) {
+
+    public static void assertEqualsReflectively(Object expected, Object actual)
+    {
         assertEqualsReflectively(new HashSet<Object>(), expected, actual);
     }
-    
-    private static void assertEqualsReflectively(Set<Object> alreadyChecked, Object expected, Object actual) {
+
+    private static void assertEqualsReflectively(Set<Object> alreadyChecked, Object expected, Object actual)
+    {
         Class<?> expectedClass = expected.getClass();
-        if (isInReflectivelyEqualsPackage(expectedClass)) {
+        if (isInReflectivelyEqualsPackage(expectedClass))
+        {
             assertSame(expectedClass, actual.getClass());
-            if (!alreadyChecked.contains( expected )) {
+            if (!alreadyChecked.contains(expected))
+            {
                 alreadyChecked.add(expected);
-                List<Field> fields = ReflectionHelper.getFields( expectedClass );
-                for (Field field : fields) {
-                    try {
+                List<Field> fields = ReflectionHelper.getFields(expectedClass);
+                for (Field field : fields)
+                {
+                    try
+                    {
                         Object expectedValue = field.get(expected);
                         Object actualValue = field.get(actual);
-                        if (expectedValue != actualValue) {
+                        if (expectedValue != actualValue)
+                        {
                             assertEqualsReflectively(alreadyChecked, expectedValue, actualValue);
                         }
-                    } catch ( IllegalArgumentException e ) {
+                    }
+                    catch (IllegalArgumentException e)
+                    {
                         throw new RuntimeException(e);
-                    } catch ( IllegalAccessException e ) {
+                    }
+                    catch (IllegalAccessException e)
+                    {
                         throw new RuntimeException(e);
-                    } catch ( AssertionFailedError e) {
-                        throw new AssertionFailedError("Field " + field.getDeclaringClass().getSimpleName() + "." +
-                                field.getName() + " not equal: " + e.getMessage());
+                    }
+                    catch (AssertionFailedError e)
+                    {
+                        throw new AssertionFailedError("Field " + field.getDeclaringClass().getSimpleName() + "." + field.getName() + " not equal: " + e.getMessage());
                     }
                 }
             }
-        } else if (expected instanceof Collection) {
+        }
+        else if (expected instanceof Collection)
+        {
             throw new UnsupportedOperationException("Not yet implemented");
-        } else {
+        }
+        else
+        {
             assertEquals(expected, actual);
         }
     }
 
-    private static boolean isInReflectivelyEqualsPackage( Class<?> klass ) {
+    private static boolean isInReflectivelyEqualsPackage(Class<?> klass)
+    {
         String className = klass.getName();
-        for (String packagePrefix : REFLECTIVE_EQUALS_EXCLUDE_PACKAGES) {
-            if (className.startsWith( packagePrefix+"." )) {
+        for (String packagePrefix : REFLECTIVE_EQUALS_EXCLUDE_PACKAGES)
+        {
+            if (className.startsWith(packagePrefix + "."))
+            {
                 return false;
             }
         }
         return true;
     }
 
-    public static void assertStartsWith( String beginning, String stringToCheck ) {
-        if (!stringToCheck.startsWith( beginning )) {
+    public static void assertStartsWith(String beginning, String stringToCheck)
+    {
+        if (!stringToCheck.startsWith(beginning))
+        {
             throw new AssertionFailedError("Expected String to start with <" + beginning + "> but was <" + stringToCheck + ">");
         }
     }
