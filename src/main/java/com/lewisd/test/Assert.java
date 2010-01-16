658e35d2b132f73123b7df894dd792cc4e9a9fab
lewisd <lewisd@4026beb2-63e4-11de-b47b-210fc227597e>
2010-01-16 12:43:32 +0000

    Adding List comparison to assertEqualsReflectively

--- src/main/java/com/lewisd/test/Assert.java
+++ src/main/java/com/lewisd/test/Assert.java
@@ -3,13 +3,15 @@ package com.lewisd.test;
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-import com.lewisd.reflection.ReflectionHelper;
-
 import junit.framework.AssertionFailedError;
+
+import com.lewisd.reflection.ReflectionHelper;
 
 public class Assert extends junit.framework.Assert {
     
@@ -26,16 +28,20 @@ public class Assert extends junit.framework.Assert {
     }
     
     private static void assertEqualsReflectively(Set<Object> alreadyChecked, Object expected, Object actual) {
+        if (expected != null && actual == null)
+        {
+            throw new AssertionFailedError("Was null, expected " + expected);
+        }
         Class<?> expectedClass = expected.getClass();
         if (isInReflectivelyEqualsPackage(expectedClass)) {
             assertSame(expectedClass, actual.getClass());
             if (!alreadyChecked.contains( expected )) {
                 alreadyChecked.add(expected);
-                List<Field> fields = ReflectionHelper.getFields( expectedClass );
+                final List<Field> fields = ReflectionHelper.getFields( expectedClass );
                 for (Field field : fields) {
                     try {
-                        Object expectedValue = field.get(expected);
-                        Object actualValue = field.get(actual);
+                        final Object expectedValue = field.get(expected);
+                        final Object actualValue = field.get(actual);
                         if (expectedValue != actualValue) {
                             assertEqualsReflectively(alreadyChecked, expectedValue, actualValue);
                         }
@@ -50,14 +56,46 @@ public class Assert extends junit.framework.Assert {
                 }
             }
         } else if (expected instanceof Collection) {
-            throw new UnsupportedOperationException("Not yet implemented");
+            if (expected instanceof List)
+            {
+                final List expectedList = (List) expected;
+                final List actualList = (List) actual;
+                final Iterator ia = actualList.iterator();
+                final Iterator ie = expectedList.iterator();
+                while(ia.hasNext() && ie.hasNext())
+                {
+                    Object expectedItem = ie.next();
+                    Object actualItem = ia.next();
+                    assertEqualsReflectively(alreadyChecked, expectedItem, actualItem);
+                }
+                if (ie.hasNext())
+                {
+                    throw new AssertionFailedError("Missing item " + ie.next() + " from list : " + actualList + ", expected " + expected);
+                }
+                if (ia.hasNext())
+                {
+                    throw new AssertionFailedError("Unexpected item " + ia.next() + " in list : " + actualList + ", expected " + expected);
+                }
+            }
+            else if (expected instanceof Set)
+            {
+                throw new UnsupportedOperationException("Unsupported collection: " + expected.getClass());
+            }
+            else if (expected instanceof Map)
+            {
+                throw new UnsupportedOperationException("Unsupported collection: " + expected.getClass());
+            }
+            else
+            {
+                throw new UnsupportedOperationException("Unsupported collection: " + expected.getClass());
+            }
         } else {
             assertEquals(expected, actual);
         }
     }
 
     private static boolean isInReflectivelyEqualsPackage( Class<?> klass ) {
-        String className = klass.getName();
+        final String className = klass.getName();
         for (String packagePrefix : REFLECTIVE_EQUALS_EXCLUDE_PACKAGES) {
             if (className.startsWith( packagePrefix+"." )) {
                 return false;
